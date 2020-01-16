487fea277ce15b7dd6a34be6d582c64a4dcc2ba5
Drake Wilcox and jeremy kale padot <drake@>
2020-01-16 15:59:51 -0800

    add functions to display and hide a celebrity based on question answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+  $("#formOne").submit(function(event)  {
+    var hunk = $("input:radio[name=hunk]:checked").val();
+    var smack = $("input:radio[name=smack]:checked").val();
+      
+  
 
+      if (hunk === "3" && smack === "3"){
+      $(".rock").show();
+      $(".ice").hide();
+      $(".busey").hide();
+         }
+      
+      else if (hunk === "2" && smack === "2"){
+        $(".ice").show();
+        $(".rock").hide();
+        $(".busey").hide();
+    }
+      else {
+        $(".busey").show();
+        $(".ice").hide();
+        $(".rock").hide();
+      }
+    event.preventDefault();
+  });
 });
